c5fa7c0e267d2ccd87659c50a0a6ea4290aa5d28
diakivm <83723897+diakivm@users.noreply.github.com>
2021-10-26 12:53:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log(sImg.src);
 
 sImg.addEventListener('click', () => {
 
-   if (sImg.src == "http://127.0.0.1:5500/img/sCat.jpg") {
+   if (sImg.src == "https://diakivm.github.io/cat/img/sCat.jpg") {
       sImg.src = "img/fCat.jpg"
    } else {
       sImg.src = "img/sCat.jpg"
