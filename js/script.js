cfe2a06c42b347d3f5db49a4eefa11737b2b2e36
diakivm <83723897+diakivm@users.noreply.github.com>
2021-10-26 12:35:47 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+let sImg = document.querySelector('#img')
+
+
+sImg.addEventListener('click', () => {
+
+   if (sImg.src == "http://127.0.0.1:5500/img/sCat.jpg") {
+      sImg.src = "img/fCat.jpg"
+   } else {
+      sImg.src = "img/sCat.jpg"
+   }
+
+})
